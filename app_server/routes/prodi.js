0a32974600735a53a250433d52ce18947a85565f
ericcandra <ericcandra_2226240059@mhs.mdp.ac.id>
2024-12-19 17:34:40 +0700

    deploy

---
+++ app_server/routes/prodi.js
@@ -0,0 +1,10 @@
+// var express = require('express');
+// var router = express.Router();
+
+// const prodiController = require('../controllers/prodiController');
+
+// router.get("/", prodiController.index);
+
+// router.post("/store", prodiController.store);
+
+// module.exports = router;
